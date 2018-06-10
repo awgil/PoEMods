dc9ca142aaa52413c346a22c6a70893320a9aee3
ag <andrew.gilewsky@gmail.com>
2018-06-10 20:06:56 +0300

    Added "automatic gun switching" mod.

---
+++ Mods/Gunslinger/SwitchAfterCombat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Patchwork.Attributes;
+using UnityEngine;
+
+// when combat ends for a PC that has a ranged weapon equipped, look for following weapons and switch to next firearm
+namespace Gunslinger
+{
+    [ModifiesType]
+    public abstract class mod_PartyMemberAI : PartyMemberAI
+    {
+        [NewMember]
+        private void add_TrySwitchingToFirearm(GameObject owner)
+        {
+            try
+            {
+                if (owner == null)
+                    return;
+
+                var ownerEquipment = owner.GetComponent<Equipment>();
+                var ownerCurItems = ownerEquipment?.CurrentItems;
+                var ownerCurAttack = ownerCurItems?.PrimaryWeapon?.GetComponent<AttackRanged>();
+                if (ownerCurAttack == null)
+                    return; // current weapon is not a ranged one, ignore
+
+                // find potential candidate for weapon switch (last weapon in "firearm chain" starting from next weapon)
+                int switchTo = ownerCurItems.SelectedWeaponSet;
+                for (int i = ownerCurItems.SelectedWeaponSet + 1; i < ownerCurItems.AlternateWeaponSets.Length; ++i)
+                {
+                    if (!ownerCurItems.AlternateWeaponSets[i]?.PrimaryWeapon?.GetComponent<AttackFirearm>())
+                        break; // chain is broken, stop
+                    switchTo = i;
+                }
+
+                if (switchTo != ownerCurItems.SelectedWeaponSet)
+                {
+                    // found something to switch to!
+                    var betterWeapon = ownerCurItems.AlternateWeaponSets[switchTo].PrimaryWeapon;
+                    Console.AddMessage($"{CharacterStats.NameColored(owner)} switches to {betterWeapon.Name} (weapon set {switchTo + 1})!");
+                    ownerEquipment.SelectWeaponSet(switchTo, true);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.AddMessage($"Gunslinger::TrySwitchingToFirearm: exception {e.Message}", e.StackTrace);
+            }
+        }
+
+        [ModifiesMember]
+        public override void HandleCombatEnd(object sender, EventArgs e)
+        {
+            if (this.m_ai == null)
+                return;
+
+            var currentState = this.m_ai.CurrentState as AI.Achievement.Attack;
+            if (currentState != null && currentState.CanUserInterrupt())
+            {
+                currentState.OnCancel();
+                this.m_ai.PopState(currentState);
+            }
+
+            // if we currently have a ranged weapon equipped, look for firearm in next slots
+            add_TrySwitchingToFirearm(this.m_ai.Owner);
+
+            // we can't really call base.HandleCombatEnd here (since base class method is what we're overriding, we'll get infinite recursion)
+            // so just duplicate it here, it's very simple
+            ReloadFirearmsInAlternateWeaponSets(this.m_ai.Owner);
+        }
+    }
+}
---
+++ Mods/Gunslinger/SwitchAfterHit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Patchwork.Attributes;
+using UnityEngine;
+
+// whenever projectile from a PC's firearm hits, look for opportunity to switch weapons (to a different ranged weapon in set with lower id)
+namespace Gunslinger
+{
+    [ModifiesType]
+    public class mod_AttackFirearm : AttackFirearm
+    {
+        [NewMember]
+        private void add_TrySwitchingToOtherWeapon()
+        {
+            try
+            {
+                var ownerPartyMemberAI = this.Owner?.GetComponent<PartyMemberAI>();
+                if (!ownerPartyMemberAI)
+                    return; // attacker isn't a party member, ignore
+
+                var ownerEquipment = this.Owner.GetComponent<Equipment>();
+                var ownerCurItems = ownerEquipment?.CurrentItems;
+                if (ownerCurItems == null || ownerCurItems.PrimaryWeapon == null || ownerCurItems.SelectedWeaponSet <= 0)
+                    return; // owner doesn't have primary weapon OR currently equipped set is first one, ignore
+
+                var ownerPrimaryAttack = ownerCurItems.PrimaryWeapon.GetComponent<AttackBase>();
+                if (ownerPrimaryAttack != this)
+                    return; // owner's current weapon is not what caused the attack, ignore
+
+                // find potential candidate for weapon switch (should be another ranged weapon that is either non-reloadable or already loaded)
+                for (int i = ownerCurItems.SelectedWeaponSet - 1; i >= 0; --i)
+                {
+                    var candidateWeapon = ownerCurItems.AlternateWeaponSets[i]?.PrimaryWeapon;
+                    var candidateAttack = candidateWeapon?.GetComponent<AttackRanged>();
+                    if (!candidateAttack)
+                        continue;
+
+                    var candidateFirearm = candidateAttack as AttackFirearm;
+                    if (candidateFirearm != null && candidateFirearm.RequiresReload)
+                        continue; // skip firearms that need reloading
+
+                    // found one!
+                    Console.AddMessage($"{CharacterStats.NameColored(this.Owner)} switches to {candidateWeapon.Name} (weapon set {i + 1})!");
+                    ownerEquipment.SelectWeaponSet(i, true);
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.AddMessage($"Gunslinger::TrySwitchingToOtherWeapon: exception {e.Message}", e.StackTrace);
+            }
+        }
+
+        [NewMember]
+        public override void OnImpact(GameObject projectile, GameObject enemy)
+        {
+            base.OnImpact(projectile, enemy); // this is ok, we're introducing new override
+            add_TrySwitchingToOtherWeapon();
+        }
+    }
+}
